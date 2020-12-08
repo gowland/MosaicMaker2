70a403bd919033418ac94c33be56f43110cf37e9
Robert Gowland <robert.gowland@sonova.com>
2020-12-08 09:05:13 -0500

    Start apply convolution kernels

--- MosaicMaker2/MosaicMaker2/MainWindow.xaml.cs
+++ MosaicMaker2/MosaicMaker2/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -36,17 +40,73 @@ namespace MosaicMaker2
         {
             var sourceImg = @"C:\src\MosaicMaker2\Alphabet\2004_Photos\100_0052.bmp";
             Class1 class1 = new Class1();
-            var imageStats = class1.GetStats(new PhysicalImage(sourceImg), new ImageManipulationInfo(0, 0, 40, 30));
+            var physicalImage = new PhysicalImage(sourceImg);
+
+            var imageStats = class1.GetStats(physicalImage, new ImageManipulationInfo(0, 0, 40, 30));
             DumpArr(imageStats.LowResR.Values);
             DumpArr(imageStats.LowResG.Values);
             DumpArr(imageStats.LowResB.Values);
             DumpArr(imageStats.LowResIntensity.Values);
-            class1.CompareImageToAlphabet(new PhysicalImage(sourceImg), new ImageManipulationInfo(0, 0, 40, 30));
+
+//            class1.CompareImageToAlphabet(physicalImage, new ImageManipulationInfo(0, 0, 40, 30));
+
+            var edge = class1.GetMidResConvolution(physicalImage);
+//            edge.Save(@"C:\src\MosaicMaker2.png");
+            EdgeDetectedBitmap = edge.ToBitmapImage();
+
         }
 
         private void DumpArr(int[] arr)
         {
             Console.WriteLine(string.Join(", ", arr));
+        }
+
+        public BitmapImage EdgeDetectedBitmap { get; set; }
+
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
+        {
+            IntPtr hBitmap = bitmap.GetHbitmap();
+            BitmapImage retval;
+
+            try
+            {
+                retval = (BitmapImage)Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(hBitmap);
+            }
+
+            return retval;
+        }
+    }
+
+    public static class BitmapExtensions
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
         }
     }
 }
